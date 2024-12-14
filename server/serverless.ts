ad670d82081f4df81de296cefd057e3fce7c4258
Jordan Nelson <jordancnelson@icloud.com>
2024-12-14 18:16:53 -0500

    add debug

--- server/serverless.ts
+++ server/serverless.ts
@@ -4,6 +4,34 @@ import { getTypeDefs } from "@/schema/schema";
 import { ApolloServer } from "@apollo/server";
 import { startServerAndCreateNextHandler } from "@as-integrations/next";
 
+import fs from "fs";
+import path from "path";
+
+/**
+ * Recursively lists all files and directories starting from the given directory.
+ * @param dirPath - The directory to start from.
+ * @param arrayOfFiles - Used for recursion to accumulate files.
+ * @returns A list of all files and directories.
+ */
+function getAllFiles(dirPath: string, arrayOfFiles: string[] = []): string[] {
+  const files = fs.readdirSync(dirPath);
+
+  files.forEach((file) => {
+    const fullPath = path.join(dirPath, file);
+    if (fs.statSync(fullPath).isDirectory()) {
+      getAllFiles(fullPath, arrayOfFiles); // Recurse into subdirectory
+    } else {
+      arrayOfFiles.push(fullPath);
+    }
+  });
+
+  return arrayOfFiles;
+}
+
+// Use the function to log all files starting from the current directory
+console.log("Recursively listing all files:");
+console.log(JSON.stringify(getAllFiles(path.resolve(__dirname)), null, 2));
+
 const main = async () => {
   const server = new ApolloServer({
     typeDefs: await getTypeDefs(),
